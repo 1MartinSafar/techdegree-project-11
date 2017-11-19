050b24ac5dc2557a173533e06bbc7760bce50281
Martin Safar <1martinsafar@gmail.com>
2017-11-19 10:27:53 +0100

    Navigation updated

--- react-flickr-gallery/src/Navigation.js
+++ react-flickr-gallery/src/Navigation.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Navigation = props => (
   <nav className="main-nav">
     <ul>
-      <li><a href='#'>Cats</a></li>
-      <li><a href='#'>Dogs</a></li>
-      <li><a href='#'>Computers</a></li>
+      <li><a href='#'>Sunset</a></li>
+      <li><a href='#'>Flowers</a></li>
+      <li><a href='#'>Clouds</a></li>
     </ul>
   </nav>
 );
