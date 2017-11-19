e1d19fc6ceb75cd023e7bbe7a3ac2e61f6725280
Martin Safar <1martinsafar@gmail.com>
2017-11-19 10:57:45 +0100

    Saving before Rebuilding Container Component

--- react-flickr-gallery/src/PhotoContainer.js
+++ react-flickr-gallery/src/PhotoContainer.js
@@ -5,6 +5,7 @@ import NotFound from './NotFound';
 
 // FLICKR URL FORMAT
 // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
 // EXAMPLE
 // https://farm1.staticflickr.com/2/1418878_1e92283336_m.jpg
 
