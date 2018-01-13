20991c367abd6abe558a14e9378a7f9548a7d5f0
Martin Safar <1martinsafar@gmail.com>
2018-01-13 11:26:42 +0100

    Updates

--- src/Components/PageNotFound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// This component will be displayed when the URL is invalid or a link is broken
-const PageNotFound = props => (
-  <h2>Page Not Found</h2>
-);
-
-export default PageNotFound;
